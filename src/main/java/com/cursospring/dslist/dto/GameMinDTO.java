7baef0f0a42ad5db6ceb6219e3e0122a645794df
Luizandro Gomes <luizandrogomes44@gmail.com>
2025-05-20 17:44:43 -0300

    Game repository, service,controller

---
+++ src/main/java/com/cursospring/dslist/dto/GameMinDTO.java
@@ -0,0 +1,41 @@
+package com.cursospring.dslist.dto;
+
+import com.cursospring.dslist.entities.Game;
+
+// ESSE ARQUIVO DTO É RESPONSÁVEL PELAS INFORMAÇÕES, TIPO AQUI VOCÊ DEFINE O QUE IRÁ APARECER NO FRONT-END
+public class GameMinDTO {
+	private Long id;
+	private String title;
+	private Integer year;
+	private String imgUrl;
+	private String shortDescription;
+	
+	
+	public GameMinDTO() {
+		
+	}
+// AQUI, JÁ QUE JÁ ESTÁ CRIADA TODAS AS INFORMACÕES ABAIXO NA PASTA GAME, SO BASTA COLOCA-LOS EM AMBIGUIDADE (EX. THIS.TITLE...)
+	public GameMinDTO(Game entity) {
+		id = entity.getId();
+		title = entity.getTitle();
+		year = entity.getYear();
+		imgUrl = entity.getImgUrl();
+		shortDescription = entity.getShortDescription();
+	}
+	public Long getId() {
+		return id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public Integer getYear() {
+		return year;
+	}
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	
+}
