4087d43015fa0a0122fe6937cf76f214d5795bd0
Luizandro Gomes <luizandrogomes44@gmail.com>
2025-05-21 15:57:15 -0300

    Get game by id

--- src/main/java/com/cursospring/dslist/services/GameService.java
+++ src/main/java/com/cursospring/dslist/services/GameService.java
@@ -2,9 +2,12 @@ package com.cursospring.dslist.services;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.cursospring.dslist.dto.GameDTO;
 import com.cursospring.dslist.dto.GameMinDTO;
 import com.cursospring.dslist.entities.Game;
 import com.cursospring.dslist.repositories.GameRepository;
@@ -14,6 +17,12 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)	
+	public GameDTO findById(Long id) {
+		Game result  = gameRepository.findById(id).get();
+		return  new GameDTO(result);
+	}
+	@Transactional(readOnly = true)	
 	public List<GameMinDTO> findAll(){
 		// OU var result =  .....
 		List<Game> result = gameRepository.findAll();
